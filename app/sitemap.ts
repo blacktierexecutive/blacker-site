2ef50fe44f9b5ef039f84d0c686f55a44c8759ed
Glary Crasto <glarycrasto@Glarys-MacBook-Pro-2.local>
2026-05-04 20:40:47 -0400

    Update sitemap with all SEO pages

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,52 +1,44 @@
-import type { MetadataRoute } from "next";
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://www.blacktierexecutive.com";
 
   const routes = [
     "",
+    "/services",
+    "/fleet",
+    "/contact",
     "/about",
     "/book",
-    "/contact",
-    "/fleet",
-    "/service-area",
-    "/services",
 
-    "/airport-shuttle-boston",
-    "/boston-airport-car-service",
-    "/boston-airport-group-shuttle",
     "/boston-charter-bus",
-    "/boston-convention-shuttle",
     "/boston-corporate-shuttle",
     "/boston-minibus-rental",
+    "/motorcoach-rental-boston",
+    "/sprinter-van-boston",
+
+    "/airport-shuttle-boston",
+    "/logan-airport-group-transportation",
+    "/boston-airport-car-service",
+
     "/cambridge-charter-bus",
     "/cambridge-corporate-shuttle",
     "/cambridge-tech-shuttle",
-    "/conference-transportation-boston",
-    "/corporate-bus-rental-boston",
-    "/employee-commuter-shuttle-boston",
-    "/event-bus-rental-boston",
-    "/hospital-transportation-boston",
-    "/hotel-shuttle-boston",
-    "/kendall-square-corporate-shuttle",
-    "/logan-airport-group-transportation",
-    "/motorcoach-rental-boston",
-    "/newton-charter-bus",
-    "/seaport-charter-bus",
-    "/seaport-corporate-shuttle",
-    "/sprinter-van-boston",
+
     "/waltham-charter-bus",
     "/waltham-corporate-bus",
     "/waltham-employee-shuttle",
-    "/watertown-corporate-shuttle",
-    "/wedding-shuttle-boston",
 
-    "/blog/boston-airport-car-service",
-    "/blog/boston-to-nyc-car-service",
-    "/blog/boston-wedding-transportation",
-    "/blog/corporate-transportation-boston",
-    "/blog/logan-airport-pickup-guide",
-    "/blog/sprinter-van-boston",
+    "/seaport-corporate-shuttle",
+    "/kendall-square-corporate-shuttle",
+    "/watertown-corporate-shuttle",
+
+    "/corporate-bus-rental-boston",
+    "/event-bus-rental-boston",
+    "/conference-transportation-boston",
+    "/hotel-shuttle-boston",
+    "/employee-commuter-shuttle-boston",
+    "/wedding-shuttle-boston",
   ];
 
   return routes.map((route) => ({
