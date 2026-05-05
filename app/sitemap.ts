f2db4f303bcc790e2171eb3825ba6f3fb3d26c00
Glary Crasto <glarycrasto@Glarys-MacBook-Pro-2.local>
2026-05-04 20:25:22 -0400

    Update sitemap with all SEO pages

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,13 +3,54 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://www.blacktierexecutive.com";
 
-  return [
-    { url: baseUrl, lastModified: new Date() },
-    { url: `${baseUrl}/services`, lastModified: new Date() },
-    { url: `${baseUrl}/fleet`, lastModified: new Date() },
-    { url: `${baseUrl}/service-area`, lastModified: new Date() },
-    { url: `${baseUrl}/about`, lastModified: new Date() },
-    { url: `${baseUrl}/contact`, lastModified: new Date() },
-    { url: `${baseUrl}/book`, lastModified: new Date() },
+  const routes = [
+    "",
+    "/about",
+    "/book",
+    "/contact",
+    "/fleet",
+    "/service-area",
+    "/services",
+
+    "/airport-shuttle-boston",
+    "/boston-airport-car-service",
+    "/boston-airport-group-shuttle",
+    "/boston-charter-bus",
+    "/boston-convention-shuttle",
+    "/boston-corporate-shuttle",
+    "/boston-minibus-rental",
+    "/cambridge-charter-bus",
+    "/cambridge-corporate-shuttle",
+    "/cambridge-tech-shuttle",
+    "/conference-transportation-boston",
+    "/corporate-bus-rental-boston",
+    "/employee-commuter-shuttle-boston",
+    "/event-bus-rental-boston",
+    "/hospital-transportation-boston",
+    "/hotel-shuttle-boston",
+    "/kendall-square-corporate-shuttle",
+    "/logan-airport-group-transportation",
+    "/motorcoach-rental-boston",
+    "/newton-charter-bus",
+    "/seaport-charter-bus",
+    "/seaport-corporate-shuttle",
+    "/sprinter-van-boston",
+    "/waltham-charter-bus",
+    "/waltham-corporate-bus",
+    "/waltham-employee-shuttle",
+    "/watertown-corporate-shuttle",
+    "/wedding-shuttle-boston",
+
+    "/blog/boston-airport-car-service",
+    "/blog/boston-to-nyc-car-service",
+    "/blog/boston-wedding-transportation",
+    "/blog/corporate-transportation-boston",
+    "/blog/logan-airport-pickup-guide",
+    "/blog/sprinter-van-boston",
   ];
+
+  return routes.map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date(),
+  }));
 }
